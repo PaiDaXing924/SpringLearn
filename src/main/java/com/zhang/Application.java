8a17c4cca02ad763fcbfb98b4a45451c4b5b027c
PaiDaXing <2388849074@qq.com>
2022-12-29 22:57:31 +0800

    配置tomcat启动，增加重写配置文件构造方法

--- src/main/java/com/zhang/Application.java
+++ src/main/java/com/zhang/Application.java
@@ -2,13 +2,20 @@ package com.zhang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+    }
+
+    //重写configure方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 
 }
