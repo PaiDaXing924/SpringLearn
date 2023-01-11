a0be38e0e3bd257f84d9385e42c5428b6822c8d6
2388849074@qq.com <zhanghao0924>
2023-01-11 17:13:34 +0800

    提交test类

---
+++ src/test/java/test.java
@@ -0,0 +1,20 @@
+public class test {
+
+    public static void main(String[] args) {
+        int interval = 0;
+        int a = 2022;
+        int b= 2021;
+        int maxDate = 0;
+        interval = a - b;
+        interval *= 365;
+        interval += 136 - 136;
+        int n = 0;
+        --a;
+        int i = b % 4;
+        b -= i == 0 ? 4 : i;
+        maxDate = n + (a - b) / 4;
+        interval += maxDate;
+        System.out.println(interval);
+    }
+
+}
